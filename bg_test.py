ff4eab65357f960f38844204dbb956530df60c19
Xu <xu@Xus-MacBook-Pro.local>
2021-11-08 20:39:05 -0500

    Test version algorithm, much more ugly

---
+++ bg_test.py
@@ -0,0 +1,104 @@
+import cv2
+import pathlib as path
+import argparse
+import numpy as np
+
+cap = cv2.imread('bg_test_1.jpg')
+
+fgbg = cv2.createBackgroundSubtractorMOG2()
+kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(3,3))
+
+while(1):
+    ret, frame = cv2.threshold(cap,127,255,cv2.THRESH_BINARY)
+
+    fgmask = fgbg.apply(frame)
+    fgmask = cv2.morphologyEx(fgmask, cv2.MORPH_OPEN, kernel)
+
+    cv2.imshow('frame', fgmask)
+    k = cv2.waitKey(30) & 0xff
+    if k == 27:
+        break
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+
+
+################################### Old Version ################################
+# #
+# # read image as greyscale
+# img_1 = cv2.imread('bg_test.jpg')
+#
+# cv2.imshow("Binary Image", img_1)
+#
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
+#
+# img_1 = cv2.imread('bg_test.jpg', 2)
+#
+# cv2.imshow("Binary Image", img_1)
+#
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
+#
+# ret, img_1 = cv2.threshold(img_1,127,255,cv2.THRESH_BINARY)
+#
+# cv2.imshow("Binary Image", img_1)
+#
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
+#
+# img_2 = cv2.imread('bg_test_1.jpg')
+#
+# cv2.imshow("Binary Image", img_2)
+#
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
+#
+# img_2 = cv2.imread('bg_test_1.jpg', 2)
+#
+# ret, img_2 = cv2.threshold(img_2,127,255,cv2.THRESH_BINARY)
+#
+# cv2.imshow("Binary Image", img_2)
+#
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
+#
+# labels = np.zeros(img_2.shape)
+# label = 1.0
+# record = {}
+#
+# for i in range(img_2.shape[0]):
+#     for j in range(img_2.shape[1]):
+#         if img_1[i][j] == img_2[i][j]:
+#             img_2[i][j] = 255
+#         if img_2[i][j] == 0:
+#             if i > 0 and labels[i - 1][j] != 0:
+#                 labels[i][j] = labels[i - 1][j]
+#                 record[str(labels[i][j])] += 1
+#             elif j > 0 and labels[i][j - 1] != 0:
+#                 labels[i][j] = labels[i][j - 1]
+#                 tmp = record[str(labels[i][j])]
+#                 record[str(labels[i][j])] = tmp + 1
+#             else:
+#                 labels[i][j] = label
+#                 record[str(label)] = 1
+#                 label += 1
+#
+# cv2.imshow("Binary Image", img_2)
+#
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
+#
+# for i in range(img_2.shape[0]):
+#     for j in range(img_2.shape[1]):
+#         if labels[i][j] != 0 and record[str(labels[i][j])] < 30:
+#             img_2[i][j] = 255
+#
+# cv2.imshow("Binary Image", img_2)
+#
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
+
+
+
--- events.py
+++ events.py
@@ -1,141 +1,241 @@
 '''
-Nils Napp Mar 2021
+Nils Napp, Xu Hai Mar 2021
 Script to group and count the entry/exit events form beetunnel filenames
 '''
 
-
+import cv2
 import pathlib as path
 import datetime as dt
 import numpy as np
+import time
 from matplotlib import pyplot as plt
 
-#Time difference for grouping images
-DELTA_T_SEC=5
+# Time difference for grouping images
+DELTA_T_SEC = 5
 
-DIR_REL='Q1/run-2105__2021-03-08_08-59-53_top-1'
-DIR_REL='N2/run_top-3___2021-03-08_09-00-22'
+#DIR_REL = 'Q1/run-2105__2021-03-08_08-59-53_top-1'
+#DIR_REL = 'N2/run_top-3___2021-03-08_09-00-22'
 
-FMT_STR='%Y-%m-%d_%H-%M-%S_%f'
+FMT_STR = '%Y-%m-%d_%H-%M-%S_%f'
 MIN_PIC = 4
 
-
-
+start_time = time.time()
 def tstr(fname):
-    parts=fname.split('_')
-    return parts[0] + '_' +  parts[1] + '_' + parts[2]
+    parts = fname.split('_')
+    return parts[0] + '_' + parts[1] + '_' + parts[2]
 
-def delta_dt_sec(dt1,dt2):
+
+def delta_dt_sec(dt1, dt2):
     delta = dt2 - dt1
-    return delta.days * 24*60*60 + delta.seconds + delta.microseconds/1000000
+    return delta.days * 24 * 60 * 60 + delta.seconds + delta.microseconds / 1000000
+
 
 def get_events(DIR_REL):
     p = path.Path(DIR_REL)
-    dtimes=[dt.datetime.strptime(tstr(f.name),FMT_STR) for f in p.glob('*.jpg')]
+    dtimes = [dt.datetime.strptime(tstr(f.name), FMT_STR) for f in p.glob('*.jpg')]
 
     dtimes.sort()
-
-
-    if len(dtimes) <=2:
-        print(f'Warning: Not enough images in "{DIR_REL}".')
+    if len(dtimes) <= 2:
+        #print(f'Warning: Not enough images in "{DIR_REL}".')
         return None
 
-    events=[0]
-    event_delta=[]
-    dt0=dtimes[0]
-    dt_last=dt0
-    pic_per_ev=[]
-    pic_cnt=0
+    events = [0]
+    event_delta = []
+    dt0 = dtimes[0]
+    dt_last = dt0
+    pic_per_ev = []
+    pic_cnt = 0
 
-    dt_list=[]
-    
+    dt_list = []
+
+    events_list = []
+    event_elements = []
     for d in dtimes:
         pic_cnt += 1
-        ev_del=delta_dt_sec(dt_last,d)
-
+        ev_del = delta_dt_sec(dt_last, d)
         if ev_del > DELTA_T_SEC:
             if pic_cnt < MIN_PIC:
-                print(f'Removing Event at {d}, less than {MIN_PIC} pictures.')
-                pic_cnt=1
+                #print(f'Removing Event at {d}, less than {MIN_PIC} pictures.')
+                pic_cnt = 1
             else:
                 pic_per_ev.append(pic_cnt)
-                pic_cnt=1
-                events.append(delta_dt_sec(dt0,d))
+                pic_cnt = 1
+                events.append(delta_dt_sec(dt0, d))
                 event_delta.append(ev_del)
+                events_list.append(event_elements)
+                event_elements = []
         else:
-            print (ev_del)
+            #print(ev_del)
+            event_elements.append(d)
             if ev_del < .2:
                 dt_list.append(ev_del)
-                    
-            
+
         dt_last = d
 
-    print(f'There were {len(events)} entry or exit events with a min delta={DELTA_T_SEC} seconds.')
-    print(f'Dt mean = {1/np.mean(dt_list)}.')
-    
-    ret={}
-    ret['events']=events
-    ret['event_delta']=event_delta
-    ret['event_cnt']=pic_per_ev
-    ret['start_dt']=dtimes[0]
-    ret['ev_dt']=dt_list
+    #print(f'There were {len(events)} entry or exit events with a min delta={DELTA_T_SEC} seconds.')
+    #print(f'Dt mean = {1 / np.mean(dt_list)}.')
+    ret = {}
+    ret['events'] = events
+    ret['event_delta'] = event_delta
+    ret['event_cnt'] = pic_per_ev
+    ret['start_dt'] = dtimes[0]
+    ret['ev_dt'] = dt_list
+    ret['events_list'] = events_list
     return ret
 
 
-r1=get_events('N2/run_top-3___2021-03-08_09-00-22')
-r2=get_events('Q1/run-2105__2021-03-08_08-59-53_top-1')
-r3=get_events('run_top-3___2021-03-12_08-50-38')
-r4=get_events('run_top-4___2021-03-12_08-50-27')
+#r1 = get_events('N2/run_top-3___2021-03-08_09-00-22')
+#r2 = get_events('Q1/run-2105__2021-03-08_08-59-53_top-1')
+#r3 = get_events('run_top-3___2021-03-12_08-50-38')
+#r4 = get_events('run_top-4___2021-03-12_08-50-27')
+
+#r4 = get_events('run_side-3___2021-08-28_12-00-48')
+#runs = [r1, r2, r3, r4]
 
 
-runs=[r1,r2,r3,r4]
+def getImage(d, f_dir, names):
+    year = str(d.year)
+    if d.month < 10:
+        month = "0" + str(d.month)
+    else:
+        month = str(d.month)
+    if d.day < 10:
+        day = "0" + str(d.day)
+    else:
+        day = str(d.day)
+    if d.hour < 10:
+        hour = "0" + str(d.hour)
+    else:
+        hour = str(d.hour)
+    if d.minute < 10:
+        minus = "0" + str(d.minute)
+    else:
+        minus = str(d.minute)
+    if d.second < 10:
+        sec = "0" + str(d.second)
+    else:
+        sec = str(d.second)
+    if d.microsecond < 10:
+        ms = "0" + str(d.microsecond)
+    else:
+        ms = str(d.microsecond)
+    name = year + "-" + month + "-" + day + "_" + hour + "-" + minus + "-" + sec + "_" + ms + "*"
 
-times=[]
+    p = path.Path(f_dir)
 
-fig=plt.figure('Event-Histogram')
+    for f in p.glob(name):
+        names.append(str(f))
 
 
-for r in runs:
-    start_dt=r['start_dt']
-    dt_8am = dt.datetime(start_dt.year,start_dt.month,start_dt.day,hour=8,minute=0)
-    extra_secs=delta_dt_sec(dt_8am,start_dt)
-    print(f'Adding {extra_secs} for starting at 8:00am, dt for run is {start_dt}.')
-    times.extend(list((t + extra_secs)/60/60 for t in r['events']))
-    
-plt.hist(times,bins=50)
-    
-plt.ylabel('Events')
-plt.xlabel('Time')
-plt.xticks(ticks=[0,2,4,6,8,10],labels=['8:00','10:00','12:00','14:00','16:00','18:00'])
-plt.title(f'Events vs. Time of Day ({len(times)} total)')
+def bg_sub(names, num):
+    first_img = names[0]
+    fir_img = cv2.imread(first_img, 2)
 
-plt.show()
+    ret, fir_img = cv2.threshold(fir_img, 127, 255, cv2.THRESH_BINARY)
 
-fig=plt.figure('Event-Histogram-Per-Day')
-for r,i in zip(runs,range(1,len(r)+1)):
-    plt.subplot(4,1,i)
-    start_dt=r['start_dt']
-    dt_8am = dt.datetime(start_dt.year,start_dt.month,start_dt.day,hour=8,minute=0)
-    extra_secs=delta_dt_sec(dt_8am,start_dt)
-    print(f'Adding {extra_secs} for starting at 8:00am, dt for run is {start_dt}.')
-    times=list((t + extra_secs)/60/60 for t in r['events'])
-    plt.hist(times,bins=50)
-    plt.ylabel(f'Events\ntotal={len(times)}')
-    plt.xlim(0,10)
-    plt.xticks(ticks=[0,2,4,6,8,10],labels=['','','','','',''])
-    
-plt.xlabel('Time',fontsize=24)
-plt.xticks(ticks=[0,2,4,6,8,10],labels=['8:00','10:00','12:00','14:00','16:00','18:00'])
-#plt.title(f'Events vs. Time of Day ({len(times)} total)')
+    img = names[num]
+    img_g = cv2.imread(img, 2)
+    ret, img = cv2.threshold(img_g, 127, 255, cv2.THRESH_BINARY)
 
-plt.show()
+    labels = np.zeros(img.shape)
+    label = 1.0
+    record = {}
 
-fig=plt.figure('Dt-frame-Per-Day')
-for r,i in zip(runs,range(1,len(r)+1)):
-    plt.subplot(4,1,i)
-    #plt.hist(list(map(lambda x : 1/(x+0.001), r['ev_dt'])),bins=50)#bins=np.linspace(0,0.2,50))
-    plt.hist(list(map(lambda x : 1/(x+0.0001), r['ev_dt'])),bins=np.linspace(0,100,40))
-    
-#plt.xlabel('Time',fontsize=24)
-#plt.title(f'Events vs. Time of Day ({len(times)} total)')
+    for i in range(img.shape[0]):
+        for j in range(img.shape[1]):
+            if fir_img[i][j] == img[i][j]:
+                img[i][j] = 255
+            if img[i][j] == 0:
+                if i > 0 and labels[i - 1][j] != 0:
+                    labels[i][j] = labels[i - 1][j]
+                    record[str(labels[i][j])] += 1
+                elif j > 0 and labels[i][j - 1] != 0:
+                    labels[i][j] = labels[i][j - 1]
+                    tmp = record[str(labels[i][j])]
+                    record[str(labels[i][j])] = tmp + 1
+                else:
+                    labels[i][j] = label
+                    record[str(label)] = 1
+                    label += 1
 
-plt.show()
+
+def determinEvent(names, num, determineE):
+    first_image_name = names[0]
+    # read image as greyscale
+    fir_img = cv2.imread(first_image_name, 2)
+
+    # Otsu's thresholding after Gaussian filtering
+    fir_blur = cv2.GaussianBlur(fir_img, (5, 5), 0)
+    fir_ret, fir_th = cv2.threshold(fir_blur, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+
+    fir_x_avg = 0
+    fir_y_avg = 0
+    fir_total = 0
+
+    for i in range(fir_th.shape[0]):
+        for j in range(fir_th.shape[1]):
+            if fir_th[i][j] == 0:
+                fir_x_avg += j
+                fir_y_avg += i
+                fir_total += 1
+
+    fir_x_avg = int(fir_x_avg / fir_total)
+    #fir_y_avg = int(fir_y_avg / fir_total)
+
+    # Second image
+    second_image_name = names[len(names) - 1]
+    # read image as greyscale
+    se_img = cv2.imread(second_image_name, 2)
+
+    # Otsu's thresholding after Gaussian filtering
+    se_blur = cv2.GaussianBlur(se_img, (5, 5), 0)
+    se_ret, se_th = cv2.threshold(se_blur, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+
+    se_x_avg = 0
+    se_y_avg = 0
+    se_total = 0
+
+    # Compute Center of mass
+    for i in range(se_th.shape[0]):
+        for j in range(se_th.shape[1]):
+            if se_th[i][j] == 0:
+                se_x_avg += j
+                se_y_avg += i
+                se_total += 1
+
+    se_x_avg = int(se_x_avg / se_total)
+    #se_y_avg = int(se_y_avg / se_total)
+
+    if se_x_avg - fir_x_avg > 0:
+        determineE.append("Right")
+    elif se_x_avg - fir_x_avg < 0:
+        determineE.append("Left")
+    else:
+        determineE.append("Error")
+
+
+dir = 'run_top-3___2021-08-28_12-00-37'
+r3 = get_events(dir)
+e_names = []
+
+determineE = []
+
+es_list = r3.get('events_list')
+nums = r3.get('event_cnt')
+
+for es in es_list:
+    names = []
+    for e in es:
+        getImage(e, dir, names)
+    e_names.append(names)
+
+for i in range(len(nums)):
+    for j in range(len(e_names[i])):
+        bg_sub(e_names[i], j)
+
+for i in range(len(nums)):
+    determinEvent(e_names[i], nums[i], determineE)
+
+print(determineE)
+print(es_list)
+print(time.time() - start_time)
